3a91a8cc5bbccb66ba0aaea9ff1b1d9b66de6c88
sssss15 <82362493+sssss15@users.noreply.github.com>
2023-04-25 11:45:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function (req, res) {
     if (req.url == '/') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is the Default Page of the Server. Sourav and sushree.</p></body></html>');
+        res.write('<html><body><p>This is the Default Page of the Server. Sourav and sushree loves each other.</p></body></html>');
         res.end();
 
     }
